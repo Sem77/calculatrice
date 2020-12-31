0025750b7e313fe8eb168cedb6d4ce2d80af34b8
Sem77 <semwong78@gmail.com>
2020-12-31 02:47:52 +0100

    suppression du texte welcome

--- src/com/sem/controller/MainController.java
+++ src/com/sem/controller/MainController.java
@@ -157,6 +157,5 @@ public class MainController implements Initializable{
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		expArtLabel.setText("");
-		System.out.println("Welcome on the calculator");
 	}
 }
